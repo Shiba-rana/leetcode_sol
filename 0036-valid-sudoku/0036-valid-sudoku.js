fb2c3fa23486776c1bef91812cbd930a260ee426
Shibananda Rana <145925275+Shiba-rana@users.noreply.github.com>
2026-09-12 11:22:50 +0530

    Time: 4 ms (71.16%), Space: 54.7 MB (99.54%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.js
@@ -0,0 +1,30 @@
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+var isValidSudoku = function(board) {
+    const seen = new Set();
+    
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            const currentVal = board[i][j];
+            
+            if (currentVal !== '.') {
+                const boxIndex = Math.floor(i / 3) * 3 + Math.floor(j / 3);
+                
+                const rowKey = `${currentVal} in row ${i}`;
+                const colKey = `${currentVal} in col ${j}`;
+                const boxKey = `${currentVal} in box ${boxIndex}`;
+                
+                if (seen.has(rowKey) || seen.has(colKey) || seen.has(boxKey)) {
+                    return false;
+                }
+                
+                seen.add(rowKey);
+                seen.add(colKey);
+                seen.add(boxKey);
+            }
+        }
+    }
+    return true;
+};
